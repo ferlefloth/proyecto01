2cffff3f99280316a30447f6fb1b19b4cdbceb00
Mello <mellolefloth@gmail.com>
2021-03-21 22:46:35 -0300

    Componentes como como manejarse con props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save o reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//También se puede laburar con componentes creados de diversas formas
+class Saludos extends Component{ constructor(){ super(); }
+
+ render(){return(<div>
+   
+    <h2>hello</h2>
+    <h2>{this.props.name} apellido : {this.props.apellido}</h2>
+   </div>)}
+
 }
+//Se puede laburar con funciónes
+const prueba = (n,a)=>{  return `Bienvenido ${n} ${a}` }
+
+
+//Esto se puede trabajar con clases.
+class App extends Component {
+
+  nombre= "mello"; //al parecer no hace falta aclarar que son tipos de variables
+  apellido = "lefloth";
+  constructor(){
+    super()
+  }
+  render(){ // cuando se labura con clases tenemos que poner este tipo de function para que se haga el renderizado
+      return( //Siempre se debe de retornar un JSX aquí adentro
+        <div>
+          <h2>{prueba(this.nombre,this.apellido)}</h2>  {/*tambien podría usarse el this.nombre en ese caso  */}
+          <Saludos name="Victor" apellido="Melhouza"/>{/*Tambien se pueden hacer con PROPS*/}
+        </div>
+      )
+  }
+}
+
+
+
+
 
 export default App;
