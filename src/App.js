4d7fd7394b16b94dbeca4ea691a923e070c49b08
Mello <mellolefloth@gmail.com>
2021-03-23 23:33:36 -0300

    Se explicó los constructores. Tener en cuenta que en los comentarios hay una seguidilla de cada ejemplo

--- src/App.js
+++ src/App.js
@@ -8,19 +8,40 @@ import { unmountComponentAtNode } from 'react-dom';
 /*los ESTADOS: Es un objeto que representa el estado actual de un componente */ 
 class Fruta extends Component {
 
-constructor(){
+constructor(){ // Es un patron creacional, solo se ejecuta una vez cuando se INSTANCIA en el DOM
   super();
+  this.sumar = this.sumar.bind(this) // """"FORMA 2 DE LOS BINDS""""": Se hacen tdos los metodos de la clase acá adentro , en vez de ponerlo dentro del botón
+  //  this.restar = this.restar.bind(this)
+
+  const metodos = ["restar"]
+
+  metodos.forEach((metodo)=>{
+
+    this[metodo]= this[metodo].bind(this) 
+  })
+
+
 
   this.state = { //Se pueden definir un objeto con las propiadeades que quereamos
     cantidad : 1,
   }
 }
 
-agregar(){
+sumar(){
 
   this.setState({cantidad : this.state.cantidad + 1})
 }
 
+restar(){
+
+  this.setState({cantidad: this.state.cantidad -1})
+
+}
+
+reset(){
+
+    this.setState({cantidad: this.state.cantidad === 0 })
+}
 render(){
 return(
 
@@ -29,10 +50,13 @@ return(
   
   <h3>{this.props.name}</h3>
   <h4>Cantidad : {this.state.cantidad}</h4>     {/*//No es buena práctica tener eventos con flechas. el setState vuelve a renderizar SOLO lo que se modificó en el elemento dentro del DOM */}
-    <button onClick={this.agregar.bind(this)}>      {/*El this.agregar no podría referirse al botón , hay que ponerle el  .bind para que el this.agregar funcione. se fuerza a que el this. agregar lo busque dentro de la clase y no dentro de el método agregar()*/}
-  
-      Agregar 
-    </button>
+    <button onClick={this.sumar}>  + </button>   {/*El this.agregar no podría referirse al botón , hay que ponerle el  .bind para que el this.agregar funcione. se fuerza a que el this. agregar lo busque dentro de la clase y no dentro de el método agregar()*/}
+    <button onClick={this.restar}>  -  </button> 
+    <button onClick={this.reset}>  Reset  </button> 
+
+    <button onClick={this.reset.bind(this)}>  Reset  </button> {/*"""""""FORMA 1 DE LOS BINDS""""""": De esta forma es un poco mas Sucio , pero está bien. Lo ideal es hacerlo dentro del constructor como se muestra en los de arriba*/ }
+     
+    
   <p>${this.props.price}</p>
 </div>
 
